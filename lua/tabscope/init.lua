311f06a98b6887bcc28b246c9494085ca8dd2120
aurora0x27 <qingfengzl0831@163.com>
2025-11-27 15:53:05 +0800

    fix buffer load

--- lua/tabscope/init.lua
+++ lua/tabscope/init.lua
@@ -35,6 +35,16 @@ end
 
 function M.setup(opts)
     config.setup(opts)
+    -- TODO: check current buffer list
+    local file_buffers = vim.tbl_filter(
+        function(buf) return vim.fn.buflisted(buf) == 1 and vim.bo[buf].buftype == "" and vim.api.nvim_buf_get_name(buf) ~= "" end,
+        vim.api.nvim_list_bufs()
+    )
+    if #file_buffers > 0 then
+        for _, buf in ipairs(file_buffers) do
+            core.add_buffer_by_id(buf)
+        end
+    end
     setup_autocmds()
     create_user_commands()
 end
